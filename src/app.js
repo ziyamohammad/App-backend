0a4bd212bc8823eb0a9a84da6e9be7f2116670d9
ziyamohammad <mohammad2311061@akgec.ac.in>
2025-10-06 03:45:47 +0530

    vr

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-
+app.set("trust proxy",1)
 app.use(
   session({
     secret:"careconnect_secret_key",
